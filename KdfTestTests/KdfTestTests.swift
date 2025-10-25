50bfbe6f4a45914fd27fab55ba59bd27ca420e4e
Kadan Stadelmann <ca333@keemail.me>
2025-10-25 10:25:19 +0200

    add xcode project files and test boilerplate

---
+++ KdfTestTests/KdfTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  KdfTestTests.swift
+//  KdfTestTests
+//
+//  Created by ca333 on 24/10/2025.
+//
+
+import Testing
+@testable import KdfTest
+
+struct KdfTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
